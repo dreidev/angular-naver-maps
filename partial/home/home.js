a61b40aa38f8ea5df445cf17d157c7eaecf13e12
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-30 03:22:55 +0200

    update demo with new configurations

--- partial/home/home.js
+++ partial/home/home.js
@@ -19,7 +19,24 @@ angular.module('ngNaverMaps').controller('HomeCtrl', ['$scope', '$sce','NgNaverM
     $scope.getPos = function(venue) {
         return [venue.lat, venue.lng];
     };
-    
+
+    $scope.infoWindoSnippet = [
+      '<div class="clearfix">',
+      '<div class="iw_inner" style="color:black;background-color:white;">',
+        '   <h3>서울특별시청</h3>',
+        '   <p style="margin:0"> Lorem ipsum silor sit amet Lorem ipsum silor sit amet',
+        // '       <img src="'+ HOME_PATH +'/img/example/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
+        '       Lorem ipsum silor sit amet Lorem ipsum silor sit amet<br />',
+        '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
+        '   </p>',
+        '</div>',
+        '<div class="clearfix">',
+        '<div style="margin: 0px; padding: 0px; width: 0px; height: 0px; position: absolute; border-width: 24px 10px 0px; border-style: solid; border-color: rgb(51, 51, 51) transparent transparent; border-image: initial; pointer-events: none; box-sizing: content-box !important; transform-origin: right bottom 0px; transform: skewX(0deg); bottom: -22px;"></div>',
+        '<div style="margin: 0px; padding: 0px; width: 0px; height: 0px; position: absolute; border-width: 24px 10px 0px; border-style: solid; border-color: rgb(255, 255, 255) transparent transparent; border-image: initial; pointer-events: none; box-sizing: content-box !important; transform-origin: right bottom 0px; transform: skewX(0deg); bottom: -22px;"></div>',
+        '</div>',
+        '</div>'
+      ].join('');
+
     $scope.toggleInfoWindow = function(index) {
         $scope.$broadcast('toggleInfoWindow', index);
     };
