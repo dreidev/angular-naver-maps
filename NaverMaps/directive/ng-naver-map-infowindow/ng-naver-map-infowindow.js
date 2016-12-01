7c4404a5d71a9b78a65399a9440417d47dccdfe9
Tokyo <ahmed.tokyo1@gmail.com>
2016-12-01 16:33:14 +0200

    documented info window code

--- NaverMaps/directive/ng-naver-map-infowindow/ng-naver-map-infowindow.js
+++ NaverMaps/directive/ng-naver-map-infowindow/ng-naver-map-infowindow.js
@@ -14,41 +14,42 @@
                 transclude: true,
                 link: function(scope, element, attrs, ngNaverMap, transclude) {
                     NgNaverMap.getMap().then(function(oMap) {
+                        // Getting Naver Instance
                         var Naver = NgNaverMap.Naver;
-
-                        var oInfoWnd = new Naver.InfoWindow({content: scope.snippet});
-
+                        // Creating Info Window instance
+                        var oInfoWnd = new Naver.InfoWindow();
+                        // Configuring Info Window instance
                         oInfoWnd.setPoint(NgNaverMap.getLatLng(eval(scope.position)));
                         oInfoWnd.setPosition({top: 40});
-
-                        oMap.addOverlay(oInfoWnd);
+                        // oInfoWnd.autoPosition();
                         oInfoWnd.setVisible(false);
-
-                        // oInfoWnd.attach('mouseleave', function(oCustomEvent) {});
+                        // Adding Info Window instance to the map
+                        oMap.addOverlay(oInfoWnd);
+                        // Tracking Info Window instance visibility
                         scope.visible = false;
-
+                        // Registered Event to toggle the Info Window instance's visibility
                         scope.$on('toggleInfoWindow', function(event, index) {
                             if (index === scope.index) {
                                 scope.visible = !scope.visible;
                                 oInfoWnd.setVisible(scope.visible);
                             }
                         });
-
+                        // Registered Event to close the Info Window instance's
                         scope.$on('closeInfoWindow', function(event, index) {
                             if (index === scope.index) {
                                 scope.visible = false;
                                 oInfoWnd.setVisible(false);
                             }
                         });
-
+                        // Registered Event to close all Info Window instances
                         // scope.$on('hideAllInfoWindows', function(event) {
                         //     oInfoWnd.setVisible(false);
                         // });
-
+                        // Registered Event click calls the scope's on-click()
                         oInfoWnd.attach('click', function() {
                             scope.onClick({model: scope.model});
                         });
-
+                        // Setting the inner content of the Info window instance using transcluded HTML
                         transclude(scope.$parent, function(clone, scope) {
                             var htmlWrapper = document.createElement('div');
                             for (var i = 0; i < clone.length; i++) {
