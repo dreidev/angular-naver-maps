6a07164c0c8269bfc6f12268980df7fa472a6923
Tokyo <ahmed.tokyo1@gmail.com>
2016-12-01 15:14:44 +0200

    add auto overflow to infowindow

--- NaverMaps/directive/ng-naver-map-infowindow/ng-naver-map-infowindow.js
+++ NaverMaps/directive/ng-naver-map-infowindow/ng-naver-map-infowindow.js
@@ -1,60 +1,62 @@
 (function() {
-    angular.module('NaverMaps').directive('ngNaverMapInfowindow', ['NgNaverMap', function(NgNaverMap) {
-        return {
-            scope: {
-              index : '=',
-              model: '=?',
-              position: '@?',
-              onClick: '&?'
-            },
-            requires: '^?ngNaverMap',
-            restrict: 'E',
-            transclude: true,
-            link: function(scope, element, attrs, ngNaverMap, transclude) {
-              NgNaverMap.getMap().then(function(oMap) {
-                  var Naver = NgNaverMap.Naver;
+    angular.module('NaverMaps').directive('ngNaverMapInfowindow', [
+        'NgNaverMap',
+        function(NgNaverMap) {
+            return {
+                scope: {
+                    index: '=',
+                    model: '=?',
+                    position: '@?',
+                    onClick: '&?'
+                },
+                requires: '^?ngNaverMap',
+                restrict: 'E',
+                transclude: true,
+                link: function(scope, element, attrs, ngNaverMap, transclude) {
+                    NgNaverMap.getMap().then(function(oMap) {
+                        var Naver = NgNaverMap.Naver;
 
-                  var oInfoWnd = new Naver.InfoWindow({
-                    content: scope.snippet
-                  });
+                        var oInfoWnd = new Naver.InfoWindow({content: scope.snippet});
 
-                  oInfoWnd.setPoint(NgNaverMap.getLatLng(eval(scope.position)));
-                  oInfoWnd.setPosition({
-                      top: 40
-                  });
+                        oInfoWnd.setPoint(NgNaverMap.getLatLng(eval(scope.position)));
+                        oInfoWnd.setPosition({top: 40});
 
-                  oMap.addOverlay(oInfoWnd);
-                  oInfoWnd.setVisible(false);
+                        oMap.addOverlay(oInfoWnd);
+                        oInfoWnd.setVisible(false);
 
-                  // oInfoWnd.attach('mouseleave', function(oCustomEvent) {});
-                  scope.visible = false;
+                        // oInfoWnd.attach('mouseleave', function(oCustomEvent) {});
+                        scope.visible = false;
 
-                  scope.$on('toggleInfoWindow', function (event, index) {
-                    if(index === scope.index){
-                      scope.visible = ! scope.visible;
-                      oInfoWnd.setVisible(scope.visible);
-                    }
-                    console.log('on toggleInfoWindow');
-                  });
+                        scope.$on('toggleInfoWindow', function(event, index) {
+                            if (index === scope.index) {
+                                scope.visible = !scope.visible;
+                                oInfoWnd.setVisible(scope.visible);
+                            }
+                            console.log('on toggleInfoWindow');
+                        });
 
-                  scope.$on('hideAllInfoWindows', function (event) {
-                      oInfoWnd.setVisible(false);
-                  });
+                        scope.$on('hideAllInfoWindows', function(event) {
+                            oInfoWnd.setVisible(false);
+                        });
 
-                 oInfoWnd.attach('click', function() { scope.onClick({ model: scope.model }); });
+                        oInfoWnd.attach('click', function() {
+                            scope.onClick({model: scope.model});
+                        });
 
-                 transclude(scope.$parent, function(clone, scope) {
-                   var htmlWrapper = document.createElement('div');
-                   for (var i = 0; i < clone.length; i++) {
-                     if(clone[i].nodeType === 1){
-                      htmlWrapper.appendChild(clone[i]);
-                     }
-                   }
-                  htmlWrapper.className = "ng-map-infowindow";
-                  oInfoWnd.setContent(htmlWrapper);
-                  });
-              });
-            }
-        };
-    }]);
+                        transclude(scope.$parent, function(clone, scope) {
+                            var htmlWrapper = document.createElement('div');
+                            for (var i = 0; i < clone.length; i++) {
+                                if (clone[i].nodeType === 1) {
+                                    htmlWrapper.appendChild(clone[i]);
+                                }
+                            }
+                            htmlWrapper.style.overflow = 'auto';
+                            htmlWrapper.className = "ng-map-infowindow";
+                            oInfoWnd.setContent(htmlWrapper);
+                        });
+                    });
+                }
+            };
+        }
+    ]);
 })();
